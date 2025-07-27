39c1bb3bf773d24df21848720af677654a2ce2c9
Suvankar Das <rohanrohan510@gmail.com>
2025-07-27 19:47:46 +0530

    email error corrected

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-const sendEmail = async ({ to, subject, text }) => {
+const sendEmail = async ({ to, subject, body }) => {
     const response = await transporter.sendMail({
         from: process.env.SENDER_EMAIL,
         to,
